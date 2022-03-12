b732219ad311701038d57c9e47b4720d430863a5
jleeson <jleeson2000@gmail.com>
2022-03-12 15:19:08 -0800

    added support for .mjs extension

--- rollup.config.js
+++ rollup.config.js
@@ -6,14 +6,11 @@ import { dependencies } from "./package.json";
 export default {
     input: "src/index.js",
     output: [
-        { file: "dist/plugin.esm.js", format: "esm" },
-        { file: "dist/plugin.cjs.js", format: "cjs", exports: "default" },
+        { file: "dist/plugin.mjs", format: "esm" },
+        { file: "dist/plugin.js", format: "cjs", exports: "default" },
     ],
     plugins: [
-        esbuild({
-            target: "es2015",
-            minify: true
-        })
+        esbuild({ target: "es2015", minify: true })
     ],
     external: Object.keys(dependencies)
 };
