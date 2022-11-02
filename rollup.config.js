1c7a9b70c46f07b58326711f79d60713ec261b28
dgrammatiko <d.grammatiko@gmail.com>
2022-11-02 10:46:39 +0100

    less

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
 /* imports */
- import esbuild from "rollup-plugin-esbuild";
- import { dependencies } from "./package.json";
+import esbuild from "rollup-plugin-esbuild";
+import { dependencies } from "./package.json";
 
- /* build config */
- export default {
-     input: "src/index.js",
-     output: [
-         { file: "dist/plugin.mjs", format: "esm" },
-         { file: "dist/plugin.js", format: "cjs", exports: "default" },
-     ],
-     plugins: [
-         esbuild({ target: "es2015", minify: true })
-     ],
-     external: Object.keys(dependencies)
- };
+/* build config */
+export default {
+    input: "src/index.js",
+    output: [
+        { file: "dist/plugin.mjs", format: "esm" },
+        { file: "dist/plugin.js", format: "cjs", exports: "default" },
+    ],
+    plugins: [
+        esbuild({ target: "es2015", minify: true })
+    ],
+    external: Object.keys(dependencies)
+};
