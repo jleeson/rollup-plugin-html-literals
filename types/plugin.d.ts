efc97210e53817117c74654c3a69682426aba4ca
jleeson <jleeson2000@gmail.com>
2025-08-28 14:08:53 -0700

    fixed types

--- types/plugin.d.ts
+++ types/plugin.d.ts
@@ -1,12 +1,12 @@
-import minify from "minify-literals";
+import { minifyHTMLLiterals as MinifyFunction, Options as MinifyOptions } from "minify-literals";
 import { TransformHook } from "rollup";
 
 declare interface Options {
     include? : string | string[];
     exclude? : string | string[];
-    options? : Partial<minify.Options>;
+    options? : Partial<MinifyOptions>;
     failOnError? : boolean;
-    minifyHTMLLiterals? : typeof minify.minifyHTMLLiterals;
+    minifyHTMLLiterals? : typeof MinifyFunction;
     filter?: (id: string) => boolean;
 }
 
